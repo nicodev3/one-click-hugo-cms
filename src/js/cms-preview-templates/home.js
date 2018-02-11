c0d163cefaf0a4ac4b643f56d7bb41a29391943b
nicodev <ndevaux@gmail.com>
2018-02-11 12:40:50 +0100

    change

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -1,9 +1,8 @@
 import React from "react";
-import format from "date-fns/format";
 
 export default class PostPreview extends React.Component {
   render() {
-    const {entry, getAsset} = this.props;
+    const {widgetFor} = this.props;
     return <div>
       { widgetFor("body") }
     </div>
