812a049ea3cbcceeee8eb44e7bc3b2b466b091f5
nicodev <ndevaux@gmail.com>
2018-02-13 12:57:15 +0100

    changes

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,9 +1,7 @@
 import React from "react";
 import CMS from "netlify-cms";
 
-import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 
 CMS.registerPreviewStyle("/css/main.css");
-CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
